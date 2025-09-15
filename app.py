aec0bf1a2e27b919c34fc3cd3d804af4fd163752
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2025-09-15 18:38:55 -0400

    Initial commit
    Initial code commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return render_template('index.html', config=config)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
